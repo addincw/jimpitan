8767a94fb9943020011dca544a1b834c0f9b4ed6
addincendekia <project.cendekia.id@gmail.com>
2024-01-16 12:02:34 +0700

    handlebars-layout: improve declaration type file

--- types/handlebars-layouts.d.ts
+++ types/handlebars-layouts.d.ts
@@ -1,1 +1,31 @@
-declare module "handlebars-layouts";
+import * as Handlebars from "handlebars";
+
+interface LayoutsOptions {
+	fn?: (context: Object) => string;
+	hash?: Object;
+}
+
+interface LayoutHelpers {
+	extend: (
+		name: string,
+		customContext: Object | null,
+		options: LayoutsOptions
+	) => string;
+	embed: () => string;
+	block: (
+		name: string,
+		options: { fn?: (context: Object) => string; data?: Object }
+	) => string;
+	content: (
+		name: string,
+		options: {
+			fn?: (context: Object) => string;
+			data?: Object;
+			hash?: { mode?: string };
+		}
+	) => void;
+}
+
+declare module "handlebars-layouts" {
+	export default function layouts(hbs: typeof Handlebars): LayoutHelpers;
+}
